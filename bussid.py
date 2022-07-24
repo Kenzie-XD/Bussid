5f671bd68163620c0cd142eb28ce221632960cf2
Kenzie-XD <109863919+Kenzie-XD@users.noreply.github.com>
2022-07-24 19:00:33 +0700

    Update bussid.py

--- bussid.py
+++ bussid.py
@@ -1,6 +1,15 @@
 import requests, json, time, threading, os
+from colorama import Fore, init
+# warna
+red = Fore.LIGHTRED_EX
+green = Fore.LIGHTGREEN_EX
+blue = Fore.LIGHTBLUE_EX
+yellow = Fore.LIGHTYELLOW_EX
+white = Fore.WHITE
+bl = Fore.BLACK
+
 record = [{'Key': {'sourceCity': 'MLG', 'destinationCity': 'SBY', 'routePassed': ['SBY', 'MLG'], 'activityRewards': None}, 'Value': 30}, {'Key': {'sourceCity': 'SBY', 'destinationCity': 'SMG', 'routePassed': ['SMG', 'SBY'], 'activityRewards': None}, 'Value': 60}, {'Key': {'sourceCity': 'MLG', 'destinationCity': 'SMG', 'routePassed': ['SMG', 'MLG'], 'activityRewards': None}, 'Value': 12}, {'Key': {'sourceCity': 'SMG', 'destinationCity': 'CBN', 'routePassed': ['CBN', 'SMG'], 'activityRewards': None}, 'Value': 50}, {'Key': {'sourceCity': 'SBY', 'destinationCity': 'CBN', 'routePassed': ['CBN', 'SBY'], 'activityRewards': None}, 'Value': 10}, {'Key': {'sourceCity': 'MLG', 'destinationCity': 'CBN', 'routePassed': ['CBN', 'MLG'], 'activityRewards': None}, 'Value': 5}, {'Key': {'sourceCity': 'CBN', 'destinationCity': 'JKT', 'routePassed': ['JKT', 'CBN'], 'activityRewards': None}, 'Value': 45}, {'Key': {'sourceCity': 'SMG', 'destinationCity': 'JKT', 'routePassed': ['JKT', 'SMG'], 'activityRewards': None}, 'Value': 9}, {'Key': {'sourceCity': 'SBY', 'destinationCity': 'JKT', 'routePassed': ['JKT', 'SBY'], 'activityRewards': None}, 'Value': 5}, {'Key': {'sourceCity': 'MLG', 'destinationCity': 'JKT', 'routePassed': ['JKT', 'MLG'], 'activityRewards': None}, 'Value': 3}, {'Key': {'sourceCity': 'JKT', 'destinationCity': 'P_Merak', 'routePassed': ['P_Merak', 'JKT'], 'activityRewards': None}, 'Value': 45}]
-auth = input('Enter x-Auth: ')
+auth = input('{red}[{white}#{red}] {white}Enter x-Auth: ')
 headers = {'User-Agent': 'UnityEngine-Unity; Version: 2018.4.26f1','X-ReportErrorAsSuccess': 'true','X-PlayFabSDK': 'UnitySDK-2.20.170411','X-Authorization': '','Content-Type': 'application/json','Content-Length': '157','Host': '4ae9.playfabapi.com'}
 headers['X-Authorization'] = auth
 counter = 0
@@ -35,7 +44,7 @@ def skip_mission(token):
 			else:
 				logs = backend_data['Logs']
 				msg = logs[len(logs)-1]['Message']
-				print(f'[{token}] {msg}')
+				print(f'{red}[{white}{token}{red}] {white}{msg}')
 	
 def pass_mission():
 	carrer = create_mission()
@@ -48,7 +57,7 @@ while 1:
 	pass_mission()
 	if counter == 10:
 		counter = 0
-		print('Sleeping 2.0 second prevent anti spam')
+		print('{white}Sleeping 2.0 second prevent anti spam')
 		time.sleep(2.0)
 	else:
 		counter += 1
