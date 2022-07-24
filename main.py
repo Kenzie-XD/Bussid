e5a2d27737f72ff9fcbb96811f9591b3476ec131
Kenzie-XD <109863919+Kenzie-XD@users.noreply.github.com>
2022-07-25 01:16:19 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,69 @@
-# compile by Dhenx-ID
-# youtube Dhenx-ID
-import base64
-exec(base64.b64decode('aW1wb3J0IHJlcXVlc3RzLCBqc29uLCB0aW1lLCB0aHJlYWRpbmcsIG9zCmltcG9ydCBweWZpZ2xldApmcm9tIGNvbG9yYW1hIGltcG9ydCBGb3JlLCBpbml0CiMgd2FybmEKcmVkID0gRm9yZS5MSUdIVFJFRF9FWApncmVlbiA9IEZvcmUuTElHSFRHUkVFTl9FWApibHVlID0gRm9yZS5MSUdIVEJMVUVfRVgKeWVsbG93ID0gRm9yZS5MSUdIVFlFTExPV19FWAp3aGl0ZSA9IEZvcmUuV0hJVEUKYmwgPSBGb3JlLkJMQUNLCgppbml0KGF1dG9yZXNldD1UcnVlKQoKZGVmIGJhbm5lcihzdHIpOgogICAgb3Muc3lzdGVtKCJjbHN8fGNsZWFyIikKICAgIF9fYmFubmVyX18gPSBweWZpZ2xldC5maWdsZXRfZm9ybWF0KHN0ciwgZm9udD0ic2xhbnQiLCBqdXN0aWZ5PSJjZW50ZXIiKQogICAgcHJpbnQocmVkICsgX19iYW5uZXJfXykKICAgIHByaW50KGYnXHRcdFx0e3JlZH1bIHt3aGl0ZX1DcmVhdGVkIEJ5IEtlbnppZS1YRCB7cmVkfV0nKQogICAgcHJpbnQoZidcdFx0e3JlZH1bIHt3aGl0ZX1UaGlzIHRvb2xzIGZvciBhdXRvIE1vbmV5IEZvciBCdXNzaWQge3JlZH0gXVxuJykKCmJhbm5lcignQlVTU0lEJykKcmVjb3JkID0gW3snS2V5Jzogeydzb3VyY2VDaXR5JzogJ01MRycsICdkZXN0aW5hdGlvbkNpdHknOiAnU0JZJywgJ3JvdXRlUGFzc2VkJzogWydTQlknLCAnTUxHJ10sICdhY3Rpdml0eVJld2FyZHMnOiBOb25lfSwgJ1ZhbHVlJzogMzB9LCB7J0tleSc6IHsnc291cmNlQ2l0eSc6ICdTQlknLCAnZGVzdGluYXRpb25DaXR5JzogJ1NNRycsICdyb3V0ZVBhc3NlZCc6IFsnU01HJywgJ1NCWSddLCAnYWN0aXZpdHlSZXdhcmRzJzogTm9uZX0sICdWYWx1ZSc6IDYwfSwgeydLZXknOiB7J3NvdXJjZUNpdHknOiAnTUxHJywgJ2Rlc3RpbmF0aW9uQ2l0eSc6ICdTTUcnLCAncm91dGVQYXNzZWQnOiBbJ1NNRycsICdNTEcnXSwgJ2FjdGl2aXR5UmV3YXJkcyc6IE5vbmV9LCAnVmFsdWUnOiAxMn0sIHsnS2V5Jzogeydzb3VyY2VDaXR5JzogJ1NNRycsICdkZXN0aW5hdGlvbkNpdHknOiAnQ0JOJywgJ3JvdXRlUGFzc2VkJzogWydDQk4nLCAnU01HJ10sICdhY3Rpdml0eVJld2FyZHMnOiBOb25lfSwgJ1ZhbHVlJzogNTB9LCB7J0tleSc6IHsnc291cmNlQ2l0eSc6ICdTQlknLCAnZGVzdGluYXRpb25DaXR5JzogJ0NCTicsICdyb3V0ZVBhc3NlZCc6IFsnQ0JOJywgJ1NCWSddLCAnYWN0aXZpdHlSZXdhcmRzJzogTm9uZX0sICdWYWx1ZSc6IDEwfSwgeydLZXknOiB7J3NvdXJjZUNpdHknOiAnTUxHJywgJ2Rlc3RpbmF0aW9uQ2l0eSc6ICdDQk4nLCAncm91dGVQYXNzZWQnOiBbJ0NCTicsICdNTEcnXSwgJ2FjdGl2aXR5UmV3YXJkcyc6IE5vbmV9LCAnVmFsdWUnOiA1fSwgeydLZXknOiB7J3NvdXJjZUNpdHknOiAnQ0JOJywgJ2Rlc3RpbmF0aW9uQ2l0eSc6ICdKS1QnLCAncm91dGVQYXNzZWQnOiBbJ0pLVCcsICdDQk4nXSwgJ2FjdGl2aXR5UmV3YXJkcyc6IE5vbmV9LCAnVmFsdWUnOiA0NX0sIHsnS2V5Jzogeydzb3VyY2VDaXR5JzogJ1NNRycsICdkZXN0aW5hdGlvbkNpdHknOiAnSktUJywgJ3JvdXRlUGFzc2VkJzogWydKS1QnLCAnU01HJ10sICdhY3Rpdml0eVJld2FyZHMnOiBOb25lfSwgJ1ZhbHVlJzogOX0sIHsnS2V5Jzogeydzb3VyY2VDaXR5JzogJ1NCWScsICdkZXN0aW5hdGlvbkNpdHknOiAnSktUJywgJ3JvdXRlUGFzc2VkJzogWydKS1QnLCAnU0JZJ10sICdhY3Rpdml0eVJld2FyZHMnOiBOb25lfSwgJ1ZhbHVlJzogNX0sIHsnS2V5Jzogeydzb3VyY2VDaXR5JzogJ01MRycsICdkZXN0aW5hdGlvbkNpdHknOiAnSktUJywgJ3JvdXRlUGFzc2VkJzogWydKS1QnLCAnTUxHJ10sICdhY3Rpdml0eVJld2FyZHMnOiBOb25lfSwgJ1ZhbHVlJzogM30sIHsnS2V5Jzogeydzb3VyY2VDaXR5JzogJ0pLVCcsICdkZXN0aW5hdGlvbkNpdHknOiAnUF9NZXJhaycsICdyb3V0ZVBhc3NlZCc6IFsnUF9NZXJhaycsICdKS1QnXSwgJ2FjdGl2aXR5UmV3YXJkcyc6IE5vbmV9LCAnVmFsdWUnOiA0NX1dCmF1dGggPSBpbnB1dChmJ3tyZWR9W3t3aGl0ZX0je3JlZH1dIHt3aGl0ZX1FbnRlciBYLUF1dGhvcml6YXRpb24gOiB7Ymx9ICcpCnByaW50KGYne3doaXRlfTzilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZB7cmVkfVt7eWVsbG93fSDigKIge2JsdWV9IFNUQVJUSU5HIHt5ZWxsb3d94oCiIHtyZWR9XXt3aGl0ZX3ilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZA+JykKcHJpbnQoJycpCmhlYWRlcnMgPSB7J1VzZXItQWdlbnQnOiAnVW5pdHlFbmdpbmUtVW5pdHk7IFZlcnNpb246IDIwMTguNC4yNmYxJywnWC1SZXBvcnRFcnJvckFzU3VjY2Vzcyc6ICd0cnVlJywnWC1QbGF5RmFiU0RLJzogJ1VuaXR5U0RLLTIuMjAuMTcwNDExJywnWC1BdXRob3JpemF0aW9uJzogJycsJ0NvbnRlbnQtVHlwZSc6ICdhcHBsaWNhdGlvbi9qc29uJywnQ29udGVudC1MZW5ndGgnOiAnMTU3JywnSG9zdCc6ICc0YWU5LnBsYXlmYWJhcGkuY29tJ30KaGVhZGVyc1snWC1BdXRob3JpemF0aW9uJ10gPSBhdXRoCmRlZiBjcmVhdGVfbWlzc2lvbigpOgoJZ2FtZV9kYXRhID0gJ3siRnVuY3Rpb25OYW1lIjoiUGxheUNhcmVlciIsIkZ1bmN0aW9uUGFyYW1ldGVyIjp7ImNpdGllcyI6WyJNTEciLCJTQlkiLCJTTUciLCJDQk4iLCJKS1QiLCJQX01lcmFrIiwiUF9CYWthdWhlbmkiLCJMUEciLCJQTEIiLCJKTUIiLCJQQlIiXX0sIlJldmlzaW9uU2VsZWN0aW9uIjoiTGl2ZSIsIlNwZWNpZmljUmV2aXNpb24iOm51bGwsIkdlbmVyYXRlUGxheVN0cmVhbUV2ZW50IjpmYWxzZX0nCglyZXNwb25zZSA9IHJlcXVlc3RzLnBvc3QoJ2h0dHBzOi8vNGFlOS5wbGF5ZmFiYXBpLmNvbS9DbGllbnQvRXhlY3V0ZUNsb3VkU2NyaXB0JywgaGVhZGVycz1oZWFkZXJzLCBkYXRhPWdhbWVfZGF0YSkudGV4dAoJaWYgcmVzcG9uc2UgIT0gJyc6CgkJcGFyc2VyID0ganNvbi5sb2FkcyhyZXNwb25zZSkKCQlpZiBwYXJzZXJbJ2NvZGUnXSA9PSA0MDE6CgkJCXJldHVybiBOb25lCgkJZWxpZiBwYXJzZXJbJ2NvZGUnXSA9PSAyMDA6CgkJCWRhdGEgPSBwYXJzZXJbJ2RhdGEnXQoJCQlpZiAiYXBpRXJyb3IiIGluIHN0cihkYXRhKToKCQkJCXJldHVybiBOb25lCgkJCWVsc2U6CgkJCQljYXJyZXIgPSBkYXRhWydGdW5jdGlvblJlc3VsdCddWydjYXJlZXJTZXNzaW9uJ10KCQkJCXJldHVybiBjYXJyZXIKCWVsc2U6CgkJcmV0dXJuIE5vbmUKCmRlZiBza2lwX21pc3Npb24odG9rZW4pOgoJZGF0YSA9IGpzb24uZHVtcHMoeyJGdW5jdGlvbk5hbWUiOiJGYXJlUGF5bWVudCIsIkZ1bmN0aW9uUGFyYW1ldGVyIjp7InJlY29yZHMiOnJlY29yZCwiYm9udXMiOlRydWUsImNhcmVlclRva2VuIjp0b2tlbiwiYWN0aXZpdHlSZXdhcmRUb2tlbiI6IntcInJld2FyZHNcIjpbXX0ifSwiUmV2aXNpb25TZWxlY3Rpb24iOiJMaXZlIiwiU3BlY2lmaWNSZXZpc2lvbiI6Tm9uZSwiR2VuZXJhdGVQbGF5U3RyZWFtRXZlbnQiOkZhbHNlfSkKCXJlc3BvbnNlID0gcmVxdWVzdHMucG9zdCgnaHR0cHM6Ly80YWU5LnBsYXlmYWJhcGkuY29tL0NsaWVudC9FeGVjdXRlQ2xvdWRTY3JpcHQnLCBoZWFkZXJzPWhlYWRlcnMsIGRhdGE9ZGF0YSkudGV4dAoJaWYgcmVzcG9uc2UgIT0gJyc6CgkJcGFyc2VyID0ganNvbi5sb2FkcyhyZXNwb25zZSkKCQlpZiBwYXJzZXJbJ2NvZGUnXSA9PSA0MDE6CgkJCXBhc3MKCQllbGlmIHBhcnNlclsnY29kZSddID09IDIwMDoKCQkJYmFja2VuZF9kYXRhID0gcGFyc2VyWydkYXRhJ10KCQkJaWYgImFwaUVycm9yIiBpbiBzdHIoYmFja2VuZF9kYXRhKToKCQkJCXBhc3MKCQkJZWxzZToKCQkJCWxvZ3MgPSBiYWNrZW5kX2RhdGFbJ0xvZ3MnXQoJCQkJbXNnID0gbG9nc1tsZW4obG9ncyktMV1bJ01lc3NhZ2UnXQoJCQkJcHJpbnQoZid7cmVkfVsge3doaXRlfXt0b2tlbn0ge3JlZH1dIHtibHVlfUdldCBNb25leSBBcyBCaWcgQXMg4oCifj4ge3JlZH17Y2FzaH0nKQoJCmRlZiBwYXNzX21pc3Npb24oKToKCWNhcnJlciA9IGNyZWF0ZV9taXNzaW9uKCkKCWlmIGNhcnJlciAhPSBOb25lOgkKCQl0b2tlbiA9IGNhcnJlclsndG9rZW4nXQoJCXNraXBfbWlzc2lvbih0b2tlbikKCgp3aGlsZSBUcnVlOgogICAgICAgIHBhc3NfbWlzc2lvbigpCg=='))
+import requests, json, time, threading, os
+import pyfiglet
+from colorama import Fore, init
+# warna
+red = Fore.LIGHTRED_EX
+green = Fore.LIGHTGREEN_EX
+blue = Fore.LIGHTBLUE_EX
+yellow = Fore.LIGHTYELLOW_EX
+white = Fore.WHITE
+bl = Fore.BLACK
+
+init(autoreset=True)
+
+def banner(str):
+    os.system("cls||clear")
+    __banner__ = pyfiglet.figlet_format(str, font="slant", justify="center")
+    print(red + __banner__)
+    print(f'\t\t\t{red}[ {white}Created By Kenzie-XD {red}]')
+    print(f'\t\t{red}[ {white}This tools for auto Money For Bussid {red} ]\n')
+
+banner('BUSSID')
+record = [{'Key': {'sourceCity': 'MLG', 'destinationCity': 'SBY', 'routePassed': ['SBY', 'MLG'], 'activityRewards': None}, 'Value': 30}, {'Key': {'sourceCity': 'SBY', 'destinationCity': 'SMG', 'routePassed': ['SMG', 'SBY'], 'activityRewards': None}, 'Value': 60}, {'Key': {'sourceCity': 'MLG', 'destinationCity': 'SMG', 'routePassed': ['SMG', 'MLG'], 'activityRewards': None}, 'Value': 12}, {'Key': {'sourceCity': 'SMG', 'destinationCity': 'CBN', 'routePassed': ['CBN', 'SMG'], 'activityRewards': None}, 'Value': 50}, {'Key': {'sourceCity': 'SBY', 'destinationCity': 'CBN', 'routePassed': ['CBN', 'SBY'], 'activityRewards': None}, 'Value': 10}, {'Key': {'sourceCity': 'MLG', 'destinationCity': 'CBN', 'routePassed': ['CBN', 'MLG'], 'activityRewards': None}, 'Value': 5}, {'Key': {'sourceCity': 'CBN', 'destinationCity': 'JKT', 'routePassed': ['JKT', 'CBN'], 'activityRewards': None}, 'Value': 45}, {'Key': {'sourceCity': 'SMG', 'destinationCity': 'JKT', 'routePassed': ['JKT', 'SMG'], 'activityRewards': None}, 'Value': 9}, {'Key': {'sourceCity': 'SBY', 'destinationCity': 'JKT', 'routePassed': ['JKT', 'SBY'], 'activityRewards': None}, 'Value': 5}, {'Key': {'sourceCity': 'MLG', 'destinationCity': 'JKT', 'routePassed': ['JKT', 'MLG'], 'activityRewards': None}, 'Value': 3}, {'Key': {'sourceCity': 'JKT', 'destinationCity': 'P_Merak', 'routePassed': ['P_Merak', 'JKT'], 'activityRewards': None}, 'Value': 45}]
+auth = input(f'{red}[{white}#{red}] {white}Enter X-Authorization : {bl} ')
+print(f'{white}<═══════════════════════════{red}[{yellow} • {blue} STARTING {yellow}• {red}]{white}═════════════════════════════>')
+print('')
+headers = {'User-Agent': 'UnityEngine-Unity; Version: 2018.4.26f1','X-ReportErrorAsSuccess': 'true','X-PlayFabSDK': 'UnitySDK-2.20.170411','X-Authorization': '','Content-Type': 'application/json','Content-Length': '157','Host': '4ae9.playfabapi.com'}
+headers['X-Authorization'] = auth
+def create_mission():
+	game_data = '{"FunctionName":"PlayCareer","FunctionParameter":{"cities":["MLG","SBY","SMG","CBN","JKT","P_Merak","P_Bakauheni","LPG","PLB","JMB","PBR"]},"RevisionSelection":"Live","SpecificRevision":null,"GeneratePlayStreamEvent":false}'
+	response = requests.post('https://4ae9.playfabapi.com/Client/ExecuteCloudScript', headers=headers, data=game_data).text
+	if response != '':
+		parser = json.loads(response)
+		if parser['code'] == 401:
+			return None
+		elif parser['code'] == 200:
+			data = parser['data']
+			if "apiError" in str(data):
+				return None
+			else:
+				carrer = data['FunctionResult']['careerSession']
+				return carrer
+	else:
+		return None
+
+def skip_mission(token):
+	data = json.dumps({"FunctionName":"FarePayment","FunctionParameter":{"records":record,"bonus":True,"careerToken":token,"activityRewardToken":"{\"rewards\":[]}"},"RevisionSelection":"Live","SpecificRevision":None,"GeneratePlayStreamEvent":False})
+	response = requests.post('https://4ae9.playfabapi.com/Client/ExecuteCloudScript', headers=headers, data=data).text
+	if response != '':
+		parser = json.loads(response)
+		if parser['code'] == 401:
+			pass
+		elif parser['code'] == 200:
+			backend_data = parser['data']
+			if "apiError" in str(backend_data):
+				pass
+			else:
+				logs = backend_data['Logs']
+				msg = logs[len(logs)-1]['Message']
+				print(f'{red}[ {white}{token} {red}] {red}{msg}')
+	
+def pass_mission():
+	carrer = create_mission()
+	if carrer != None:	
+		token = carrer['token']
+		skip_mission(token)
+
+
+while True:
+        pass_mission()
