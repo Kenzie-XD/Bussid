39c256cbb3ddea4708fb2abafe027b19412c6aa3
Kenzie-XD <109863919+Kenzie-XD@users.noreply.github.com>
2022-07-24 18:30:28 +0700

    Update bussid.py

--- bussid.py
+++ bussid.py
@@ -48,7 +48,7 @@ while 1:
 	pass_mission()
 	if counter == 3:
 		counter = 0
-		print('Sleeping 3.5 second prevent anti spam')
-		time.sleep(3.5)
+		print('Sleeping 2.0 second prevent anti spam')
+		time.sleep(2.0)
 	else:
 		counter += 1
