da718c04a663098c82a296ef72c790bbfc4ef708
Kenzie-XD <109863919+Kenzie-XD@users.noreply.github.com>
2022-07-25 15:32:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,62 @@
-try:
-   import base64,marshal,os,click
-   from uncompyle6 import main
-except ImportError:
-   os.system("pip3 install click uncompyle6 &> /dev/null")
-class Com:
-        def __init__(self,fil,jum):
-                self.file=fil
-                self.cout=0
-                self.jml=jum
-                self.mars(open(fil,'r').read())
+#!/usr/bin/python3
 
-        def mars(self,strg):
-                x=compile(strg,'<script>','exec')
-                xx=marshal.dumps(x)
-                xxx="#Compiled By Aslanz\n#https://youtube.com/aslanz\n\nimp>
-                if self.cout == self.jml:
-                        with open(self.file.replace('.py','_comlap.py'),'w') >
-                                com.write(xxx)
-                        print(f"\033[1;32m[+] \033[1;37mSuccessfully seved as>
-                        return True
-                self.bes(xxx)
+import requests, json, time, threading, os
+import pyfiglet
+from colorama import Fore, init
+# warna
+red = Fore.LIGHTRED_EX
+green = Fore.LIGHTGREEN_EX
+blue = Fore.LIGHTBLUE_EX
+yellow = Fore.LIGHTYELLOW_EX
+white = Fore.WHITE
+bl = Fore.BLACK
 
-        def bes(self,strg):
-                en=base64.b64encode(bytes(strg,'utf-8'))
-                de=f"#Compiled By Aslanz\n#https://youtube.com/aslanz\nimport>
-                self.cout+=1
-                self.mars(de)
+init(autoreset=True)
 
-try:
-        ofile=input("\033[1;33m[+] \033[1;37mScript > \033[1;32m")
-        juml=int(input("\033[1;33m[+] \033[1;37mCount > \033[1;32m"))
-        if juml > 100:
-                click.pause("\033[1;31m[!] Warning Script error")
-        Com(ofile,juml)
-        pil=input("\033[1;33m[+] \033[1;37mCompile bytes code (y/n) > \033[1;>
-        if pil.lower() == 'y':
-                main.compile_file(ofile.replace('.py','_enc.py'))
-                print("\033[1;32m[+] \033[1;37mDone")
-        else:
-                print("\033[1;32m[+] \033[1;37mDone")
-except KeyboardInterrupt:
-        exit()
-except Exception as F:
-        exit()
+os.system('clear')
+record = [{'Key': {'sourceCity': 'MLG', 'destinationCity': 'SBY', 'routePassed': ['SBY', 'MLG'], 'activityRewards': None}, 'Value': 30}, {'Key': {'sourceCity': 'SBY', 'destinationCity': 'SMG', 'routePassed': ['SMG', 'SBY'], 'activityRewards': None}, 'Value': 60}, {'Key': {'sourceCity': 'MLG', 'destinationCity': 'SMG', 'routePassed': ['SMG', 'MLG'], 'activityRewards': None}, 'Value': 12}, {'Key': {'sourceCity': 'SMG', 'destinationCity': 'CBN', 'routePassed': ['CBN', 'SMG'], 'activityRewards': None}, 'Value': 50}, {'Key': {'sourceCity': 'SBY', 'destinationCity': 'CBN', 'routePassed': ['CBN', 'SBY'], 'activityRewards': None}, 'Value': 10}, {'Key': {'sourceCity': 'MLG', 'destinationCity': 'CBN', 'routePassed': ['CBN', 'MLG'], 'activityRewards': None}, 'Value': 5}, {'Key': {'sourceCity': 'CBN', 'destinationCity': 'JKT', 'routePassed': ['JKT', 'CBN'], 'activityRewards': None}, 'Value': 45}, {'Key': {'sourceCity': 'SMG', 'destinationCity': 'JKT', 'routePassed': ['JKT', 'SMG'], 'activityRewards': None}, 'Value': 9}, {'Key': {'sourceCity': 'SBY', 'destinationCity': 'JKT', 'routePassed': ['JKT', 'SBY'], 'activityRewards': None}, 'Value': 5}, {'Key': {'sourceCity': 'MLG', 'destinationCity': 'JKT', 'routePassed': ['JKT', 'MLG'], 'activityRewards': None}, 'Value': 3}, {'Key': {'sourceCity': 'JKT', 'destinationCity': 'P_Merak', 'routePassed': ['P_Merak', 'JKT'], 'activityRewards': None}, 'Value': 45}]
+auth = input(f'{red}[{white}#{red}] {white}Enter X-Authorization : {bl} ')
+print(f'{white}<═══════════════════════════{red}[{yellow} • {blue} STARTING {yellow}• {red}]{white}═════════════════════════════>')
+print('')
+headers = {'User-Agent': 'UnityEngine-Unity; Version: 2018.4.26f1','X-ReportErrorAsSuccess': 'true','X-PlayFabSDK': 'UnitySDK-2.20.170411','X-Authorization': '','Content-Type': 'application/json','Content-Length': '157','Host': '4ae9.playfabapi.com'}
+headers['X-Authorization'] = auth
+def create_mission():
+	game_data = '{"FunctionName":"PlayCareer","FunctionParameter":{"cities":["MLG","SBY","SMG","CBN","JKT","P_Merak","P_Bakauheni","LPG","PLB","JMB","PBR"]},"RevisionSelection":"Live","SpecificRevision":null,"GeneratePlayStreamEvent":false}'
+	response = requests.post('https://4ae9.playfabapi.com/Client/ExecuteCloudScript', headers=headers, data=game_data).text
+	if response != '':
+		parser = json.loads(response)
+		if parser['code'] == 401:
+			return None
+		elif parser['code'] == 200:
+			data = parser['data']
+			if "apiError" in str(data):
+				return None
+			else:
+				carrer = data['FunctionResult']['careerSession']
+				return carrer
+	else:
+		return None
+
+def skip_mission(token):
+	data = json.dumps({"FunctionName":"FarePayment","FunctionParameter":{"records":record,"bonus":True,"careerToken":token,"activityRewardToken":"{\"rewards\":[]}"},"RevisionSelection":"Live","SpecificRevision":None,"GeneratePlayStreamEvent":False})
+	response = requests.post('https://4ae9.playfabapi.com/Client/ExecuteCloudScript', headers=headers, data=data).text
+	if response != '':
+		parser = json.loads(response)
+		if parser['code'] == 401:
+			pass
+		elif parser['code'] == 200:
+			backend_data = parser['data']
+			if "apiError" in str(backend_data):
+				pass
+			else:
+				logs = backend_data['Logs']
+				cash = logs[len(logs)-1]['Message']
+				print(f'{yellow}Get Bussid Money •> {white}{cash}')
+
+def pass_mission():
+	carrer = create_mission()
+	if carrer != None:	
+		token = carrer['token']
+		skip_mission(token)	
+while True:
+	pass_mission()
